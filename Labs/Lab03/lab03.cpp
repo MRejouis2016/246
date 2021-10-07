db1aa02b1ff6bbd4f61d8e775b95957ab5dca01f
Mcvoltrudge Rejouis <37338175+MRejouis2016@users.noreply.github.com>
2021-10-07 17:51:39 -0400

    Update lab03.cpp

--- Labs/Lab03/lab03.cpp
+++ Labs/Lab03/lab03.cpp
@@ -3,8 +3,48 @@
 #include "Array.h"
 
 /*Function Definitions Here*/
+template <typename T>
+void ArrayFill(Array<T>& data, const T& value)
+{
+	data[i] = value;
+}
+
+template <typename T>
+void SubArray(Array<T>& data, Array<T>&subdata, ulong a, ulong b)
+{
+	Array<T> temp = subdata;
+	if (data[i]==a && data[i]==b)
+	{
+		subdata = Array<T> (data.Size()+1);
+		data[i] = subdata[i];
+	}
+}
+
+template <typename T>
+ulong DelimitedSearch(Array<T>& data, const T& delimit, const T& target)
+{
+	for (ulong i=0; i<size; i+=1)
+	{
+	   if (data[i]!=delimit || data[i]!=target)
+	   {
+		return size;
+	   }
+	   else
+	   {
+		return i;
+	   }
+	}
+}
 
 int main()
 {
+  int data, value, a, b, size, target, delimit;
+  string value;
+  size = sizeof(arr) / sizeof(arr[0]);
+  
+  ArrayFill(value);
+	SubArray(data, subdata, a, b);
+	DelimitedSearch(data, delimit, target);
+	
 	return 0;
 }
