e68330be131db418408e22efcaaf12cdef044cf8
Mcvoltrudge Rejouis <37338175+MRejouis2016@users.noreply.github.com>
2021-11-30 17:50:12 -0500

    Update main.cpp

--- Exams/Exam03/main.cpp
+++ Exams/Exam03/main.cpp
@@ -7,14 +7,24 @@
 using namespace std;
 using namespace ds;
 
+//Mac Rejouis
 /****************************************************************************************
  * Fundamentals
  ****************************************************************************************
- *
- *
- *
- *
- *
+ //#1
+ * a.) A queue follows the principle First-In-First-Out (FIFO).
+ 
+ * b.) In terms of hashing, a collision occurs when multiple keys are mapped to the same
+ hash value.
+ 
+ * c.) For a map data structure, you need an array or linked list of key-value pairs in order
+ to represent a map.
+ 
+ * d.) The load factor is equal to k = (m/n) where m is the number of stored keys and n is
+ the number of slots in a hash table.
+ 
+ * e.) The insertion method handles collision by open addressing, which is shifting a key to
+ the next available slot.
  *
  *
  *
@@ -24,16 +34,73 @@ using namespace ds;
 /****************************************************************************************
  * Implementation
  ***************************************************************************************/
-
+//#2
+template <class T>
+void RemoveFromBack()
+{
+  if (back!=NULL)
+   {
+	 Node<T>* t = back;
+    	 back=back->prev;
+    	 delete t;
+    	 t=NULL;
+    	 if (back==NULL)
+    	 {
+      	   front=NULL;
+    	 }
+    	else
+  	{
+      	  back->next = NULL;
+    	}
+   }
+}
 
 
 /****************************************************************************************
  * Problem Solving
  ***************************************************************************************/
+//#3
+int OddMedian(Array<int>&data)
+{
+  int size;
+  int median = (arr[i]+arr[size-1])/2;
+  for (int i=0; i<size; i++)
+  {
+    if (data.size%2!=0)
+    {
+	return median;
+    }
+  }
+  return 0;
+}
 
-
-
+//#4
+string Alter(string str)
+{
+   stack<char> s;
+   int n = str.length();
+   for (int i=0; i<n; i++)
+   {
+     s.Push();
+   }
+   for (int i=0; i<n; i++)
+   {
+     if (str[i]==str[i+1])
+     {
+	s.Pop();
+     }
+     else
+     {
+	return false;
+     }
+   }
+   return str;
+}
+		
 int main()
 {
+	string str = "apple";
+	Alter(str)
+	cout << str;
 	return 0;
 }
